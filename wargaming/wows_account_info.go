d51726223eaf743dbcb27369392e07ad2f983c92
kakwa <carpentier.pf@gmail.com>
2023-01-11 00:45:59 +0100

    Q & D fix for accountinfo

--- wargaming/wows_account_info.go
+++ wargaming/wows_account_info.go
@@ -17,7 +17,7 @@ import (
 //     Valid realms: RealmAsia, RealmEu, RealmNa
 // accountId:
 //     Player account ID. Maximum limit: 100. Min value is 1.
-func (service *WowsService) AccountInfo(ctx context.Context, realm Realm, accountId []int, options *wows.AccountInfoOptions) (*wows.AccountInfo, error) {
+func (service *WowsService) AccountInfo(ctx context.Context, realm Realm, accountId []int, options *wows.AccountInfoOptions) (map[int]*wows.AccountInfo, error) {
 	if err := validateRealm(realm, []Realm{RealmAsia, RealmEu, RealmNa}); err != nil {
 		return nil, err
 	}
@@ -41,7 +41,7 @@ func (service *WowsService) AccountInfo(ctx context.Context, realm Realm, accoun
 		}
 	}
 
-	var data *wows.AccountInfo
+	var data map[int]*wows.AccountInfo
 	err := service.client.getRequest(ctx, sectionWows, realm, "/account/info/", reqParam, &data, nil)
 	return data, err
 }
